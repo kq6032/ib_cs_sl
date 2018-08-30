552c41c99f556c712270c5dec84fc82252b2383d
kq6032 <kevin.z.q77@gmail.com>
2018-08-30 13:41:27 -0700

    Finished lab 2 and started lab 3

---
+++ sl1_2/lab2/gradeCalc.java
@@ -0,0 +1,30 @@
+/*
+Author: Kevin Qiu
+Class: IB Computer Science 1-2 Period 3
+Last Edited: Aug. 29, 2018
+*/
+import java.util.Scanner;
+
+class gradeCalc {
+    public static void main(String args[]) {
+
+        float grade = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please input a grade to change to a letter");
+        grade = input.nextFloat();
+
+        if (grade >= 90) {
+            System.out.println("The grade is an A");
+        } else if (grade < 90 && grade >= 80) {
+            System.out.println("The grade is a B");
+        } else if (grade < 80 && grade >= 70) {
+            System.out.println("The grade is a C");
+        } else if (grade < 70 && grade >= 60) {
+            System.out.println("The grade is a D");
+        } else {
+            System.out.println("The grade is an F");
+        }
+    }
+}
---
+++ sl1_2/lab3/quadraticRoots.java
@@ -0,0 +1,29 @@
+/*
+Author: Kevin Qiu
+Class: IB Computer Science 1-2 Period 3
+Last Edited: Aug. 29, 2018
+*/
+
+import java.util.Scanner;
+
+class quadraticRoots {
+    public static void main(String Args[]) {
+
+        /*
+         Ax^2 + Bx + C = 0
+         coeff1 = A
+         coeff2 = B
+         const = C
+        */
+        double coeff1 = 0;
+        double coeff2 = 0;
+        double constant = 0;
+        double root1 = ((-coeff2 + Math.sqrt(Math.pow(coeff2, 2) - (4 * coeff1 * constant))) / (2 * coeff1));
+        double root2 = ((-coeff2 - Math.sqrt(Math.pow(coeff2, 2) - (4 * coeff1 * constant))) / (2 * coeff1));
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please")
+
+
+    }
+}
