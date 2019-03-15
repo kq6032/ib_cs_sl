23b429e6bce3d1b1669e596c08982ce4521b8baf
Kevin Q <kevin.z.q77@gmail.com>
2019-03-15 12:19:03 -0700

    Completed lab10

---
+++ src/com/sl1_2/lab10/lab10.java
@@ -0,0 +1,145 @@
+package com.sl1_2.lab10;
+/*
+Author: Kevin Qiu
+Class: IB Computer Science 1-2 Period 3
+Last Edited: Mar. 7, 2019
+*/
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+
+public class lab10 extends Application {
+    private static int colorCounter = 0;
+    private static int scaleCounter = 0;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public static void setColors(Color clr, Circle cl, Line ln, Rectangle fillRect, Rectangle empRect, Ellipse el, Text tx) {
+        cl.setStroke(clr);
+        ln.setStroke(clr);
+        fillRect.setStroke(clr);
+        fillRect.setFill(clr);
+        empRect.setStroke(clr);
+        el.setStroke(clr);
+        el.setFill(clr);
+        tx.setStroke(clr);
+    }
+
+    public static void setScale(float scale, Circle cl, Line ln, Rectangle fillRect, Rectangle empRect, Ellipse el, Text tx) {
+        cl.setCenterX(200f * scale);
+        cl.setCenterY(200 * scale);
+        cl.setRadius(100 * scale);
+
+        ln.setStartX(200f * scale);
+        ln.setStartY(200f * scale);
+        ln.setEndX(20f * scale);
+        ln.setEndY(20f * scale);
+
+        fillRect.setX(100f * scale);
+        fillRect.setY(310f * scale);
+        fillRect.setHeight(20f * scale);
+        fillRect.setWidth(250f * scale);
+
+        empRect.setX(25f * scale);
+        empRect.setY(200f * scale);
+        empRect.setWidth(40f * scale);
+        empRect.setHeight(150f * scale);
+
+        el.setCenterX(45f * scale);
+        el.setCenterY(40f * scale);
+        el.setRadiusX(20f * scale);
+        el.setRadiusY(15f * scale);
+
+        tx.setFont(new Font(20 * scale));
+        tx.setX(70f * scale);
+        tx.setY(55f * scale);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 400);
+
+        primaryStage.setTitle("Kevin's Lab 10");
+
+        Circle cl = new Circle();
+        cl.setFill(Color.WHITE);
+        root.getChildren().add(cl);
+
+        Line ln = new Line();
+        root.getChildren().add(ln);
+
+        Rectangle fillRec = new Rectangle();
+        root.getChildren().add(fillRec);
+
+        Rectangle empRect = new Rectangle();
+        empRect.setFill(Color.WHITE);
+        root.getChildren().add(empRect);
+
+        Ellipse el = new Ellipse();
+        root.getChildren().add(el);
+
+        Text tx = new Text("This is drawn on the canvas.");
+        tx.setFont(new Font(20));
+        root.getChildren().add(tx);
+
+        setScale(1f, cl, ln, fillRec, empRect, el, tx);
+        setColors(Color.BLACK, cl, ln, fillRec, empRect, el, tx);
+
+        Button colorBtn = new Button();
+        colorBtn.setLayoutX(115);
+        colorBtn.setLayoutY(350);
+        colorBtn.setText("Change Color");
+        colorBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                colorCounter++;
+
+                if (colorCounter == 1) {
+                    setColors(Color.RED, cl, ln, fillRec, empRect, el, tx);
+                } else if (colorCounter == 2) {
+                    setColors(Color.BLUE, cl, ln, fillRec, empRect, el, tx);
+                } else if (colorCounter == 3) {
+                    setColors(Color.GREEN, cl, ln, fillRec, empRect, el, tx);
+                } else {
+                    colorCounter = 0;
+                    setColors(Color.BLACK, cl, ln, fillRec, empRect, el, tx);
+                }
+            }
+        });
+        root.getChildren().add(colorBtn);
+
+        Button scaleBtn = new Button();
+        scaleBtn.setLayoutX(215);
+        scaleBtn.setLayoutY(350);
+        scaleBtn.setText("Change Scale");
+        scaleBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                scaleCounter++;
+
+                if (scaleCounter == 1) {
+                    setScale(0.25f, cl, ln, fillRec, empRect, el, tx);
+                } else if (scaleCounter == 2) {
+                    setScale(0.5f, cl, ln, fillRec, empRect, el, tx);
+                } else {
+                    scaleCounter = 0;
+                    setScale(1f, cl, ln, fillRec, empRect, el, tx);
+                }
+            }
+        });
+        root.getChildren().add(scaleBtn);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
